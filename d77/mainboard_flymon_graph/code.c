af925e9dd81f63a2b14002c0248c59d9510bf023
Dmi77 <dlypokupo@mail.ru>
2017-02-05 15:07:39 +0300

    Update code.c

--- d77/mainboard_flymon_graph/code.c
+++ d77/mainboard_flymon_graph/code.c
@@ -3,5 +3,5 @@ void ICACHE_FLASH_ATTR startfunc(){
 void ICACHE_FLASH_ATTR timerfunc(uint32_t  timersrc) {
 }
 void webfunc(char *pbuf) {
-os_sprintf(HTTPBUFF,"<br><iframe src='http://monitor.flymon.net/console.html?macs=5CCF7F0F71E6&graphs=temp,gpio&period=1h' style='width:100%;height:320px;border:0;'></iframe>");
+os_sprintf(HTTPBUFF,"<br><iframe src='http://monitor.flymon.net/console.html?macs=ВПИСАТЬ_МАК_АДРЕС_МОДУЛЯ&graphs=temp,gpio&period=1h' style='width:100%;height:320px;border:0;'></iframe>");
 }

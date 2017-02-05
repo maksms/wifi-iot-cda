2c5818e1b6fd25037f3d2e4884438f3604bd72b6
Dmi77 <dlypokupo@mail.ru>
2017-02-05 16:35:51 +0300

    Update code.c

--- d77/mainboard_yandex_weather/code.c
+++ d77/mainboard_yandex_weather/code.c
@@ -1,2 +1,7 @@
-
-Вписать код
+void ICACHE_FLASH_ATTR startfunc(){
+}
+void ICACHE_FLASH_ATTR timerfunc(uint32_t  timersrc) {
+}
+void webfunc(char *pbuf) {
+	os_sprintf(HTTPBUFF,"<br><img src='https://info.weather.yandex.net/saint-petersburg/4.png' style='width:100%;height:320px;border:0;'></iframe>");
+}

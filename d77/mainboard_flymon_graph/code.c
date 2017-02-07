cbf9da4d46e4be36e46f71d021b67ffe2e5f6691
Dmi77 <dlypokupo@mail.ru>
2017-02-07 20:02:26 +0300

    Update code.c

--- d77/mainboard_flymon_graph/code.c
+++ d77/mainboard_flymon_graph/code.c
@@ -3,5 +3,9 @@ void ICACHE_FLASH_ATTR startfunc(){
 void ICACHE_FLASH_ATTR timerfunc(uint32_t  timersrc) {
 }
 void webfunc(char *pbuf) {
-os_sprintf(HTTPBUFF,"<br><iframe src='http://monitor.flymon.net/console.html?macs=ВПИСАТЬ_МАК_АДРЕС_МОДУЛЯ&graphs=temp,gpio&period=1h' style='width:100%;height:320px;border:0;'></iframe>");
+    char hwaddr[6];
+    char data[15];
+    wifi_get_macaddr(0, hwaddr);
+    os_sprintf(data,MACSTRT,MAC2STRT(hwaddr));
+    os_sprintf(HTTPBUFF,"<br><iframe src='http://monitor.flymon.net/console.html?macs=%s&period=1h' style='width:100%;height:320px;border:0;'></iframe>",data);
 }

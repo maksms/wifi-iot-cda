40aa904d03776bbb545c26cb1198ffc2bd2095c0
Dmi77 <dlypokupo@mail.ru>
2017-02-20 01:49:02 +0300

    Update code.c

--- d77/wifi_restart/code.c
+++ d77/wifi_restart/code.c
@@ -1,18 +1,12 @@
-uint8_t  zadergka_reboot=0;
 void ICACHE_FLASH_ATTR
 startfunc()
 {
 }
 void ICACHE_FLASH_ATTR timerfunc(uint32_t  timersrc) 
 {
-    if ( !timersrc && !timersrc%60 )  // выполнение кода каждые 60 секунд
-    {
-       if ( mtest == 15 && !mtest2  && !mtest3 && wfrc < 1000 ) zadergka_reboot = 0 ;
-       if ( mtest != 15 || mtest2 != 0 || mtest3 != 0 || 1000 < wfrc ) zadergka_reboot ++ ;
-       if ( 5 <= zadergka_reboot ) system_restart() ;
-    }
+    if ( timersrc!=0 && timersrc%300==0 && mtest != 15 && 1000 < wfrc ) system_restart() ;
 }
 void webfunc(char *pbuf) 
 {
-    os_sprintf(HTTPBUFF,"<br>mqtt: ( State: %d &frasl; Errors: %d &frasl; %d ) , reconnect: %d ", mtest , mtest2 , mtest3 , wfrc );
+    os_sprintf(HTTPBUFF,"<br>mqtt state: %s , reconnect: %d ", mtest==15?"Ok":"!" , wfrc );
 }

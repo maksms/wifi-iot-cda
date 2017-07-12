130981206e7ac8bf52eb451f69f1bd158a110eee
rapid32 <radiofanat@list.ru>
2017-07-12 22:49:06 +0300

    Update code.c
    Добавил поддержку второго счетчика PCF8583, а также поправил формулу для вычисления показаний

--- rapid/esp8266/hardware_pcf8583/code.c
+++ rapid/esp8266/hardware_pcf8583/code.c
@@ -1,151 +1,185 @@
-uint32_t current_value; 				// Инициализация переменной текущего значения счетчика
-uint32_t previous_value;				// Инициализация переменной предыдущего значения счетчика
-uint32_t counter_overflows;			// Инициализация счетчика переполнений
-uint32_t division_ratio;			  // Инициализация коэффициента деления
-uint32_t initial_indication;		// Инициализация переменной начального показания счетчика
-uint8_t value_1,value_2,value_3;
+uint32_t current_value; 	       	// Инициализация  текущего значения счетчика
+uint32_t previous_value;	       	// Инициализация предыдущего значения счетчика
+uint32_t counter_overflows;   		// Инициализация счетчика переполнений
+uint32_t division_ratio;	      	// Инициализация коэффициента деления
+uint32_t initial_indication;       	// Инициализация начального показания счетчика
+uint32_t div1;	                	// Инициализация коэффициента деления
+uint32_t init1;             		// Инициализация начального показания счетчика
+uint32_t counter1_print; 	       	// Значение показаний счетчика 1
+uint32_t counter2_print; 	       	// Значение показаний счетчика 2
+uint8_t value_1,value_2, value_3;	// Переменные value_1,value_2, value_3
 
 void ICACHE_FLASH_ATTR
 startfunc()
 {  
-// выполняется один раз при старте модуля.
-
-}
+// выполняется один раз при старте модуля
+ }
 
 PCF8583_read_my(uint8_t adr)
  {
-i2c_start();										// Устанавливаем на шине Start состояние
-i2c_writeByte(adr*2);						// отправляем байт на шину 0b101000yх (откликается любой RTC)
-  if(i2c_getAck()){return(-1);} // slave не откликнулся
-i2c_writeByte(01);							// устанавливаем регистр для чтения
-  if(i2c_getAck()){return(-1);} // slave not ack
-i2c_stop();											// Формируем на шине Stop состояние
-// ------------------------------------------------------
-i2c_start(); 										// Устанавливаем на шине Start состояние
- i2c_writeByte(adr*2+1);
-   if(i2c_getAck()){return(-1);} // slave не откликнулся
-uint8_t Val;
-Val= i2c_readByte(); //ACK
-current_value=(Val&15) + (Val/16)*10; // Поразрядное логическое "И" +
+i2c_start();				   		// Устанавливаем на шине Start состояние
+i2c_writeByte(adr*2);		   		// отправляем байт  0b101000yх (отклик любой RTC)
+     if(i2c_getAck()){return(-1);}  // slave не откликнулся
+i2c_writeByte(01);		           	// устанавливаем регистр для чтения
+     if(i2c_getAck()){return(-1);}  // slave not ack
+i2c_stop();			           		// Формируем на шине Stop состояние
+// ------------ читаем текущее значение (current_value) ----------------
+i2c_start(); 							// Устанавливаем на шине Start состояние 
+i2c_writeByte(adr*2+1);
+     if(i2c_getAck()){return(-1);} 		// slave не откликнулся
+uint8_t Val=0;							// Инициализация вспомогательной переменной
+Val= i2c_readByte(); 					// ACK
+current_value=(Val&15) + (Val/16)*10; 	// Поразрядное логическое "И" +
 value_1=Val;
 i2c_setAck(0);
-Val= i2c_readByte(); //ACK
+Val= i2c_readByte(); 					//ACK
 current_value=current_value+ (Val&15)*100 + (Val/16)*1000;
 value_2=Val;
 i2c_setAck(0);
-Val= i2c_readByte(); //ACK
+Val= i2c_readByte(); 					//ACK
 current_value=current_value+ (Val&15)*10000 + (Val/16)*100000;
 value_3=Val;
 i2c_setAck(0);
-Val= i2c_readByte(); //ACK
-previous_value=(Val&15) + (Val/16)*10; // Поразрядное логическое "И" +
+
+// -------- читаем предыдущее значение (previous_value) ------------
+Val= i2c_readByte(); 					//ACK
+previous_value=(Val&15) + (Val/16)*10; 
 i2c_setAck(0);
-Val= i2c_readByte(); //ACK
+Val= i2c_readByte(); 					//ACK
 previous_value=previous_value+ (Val&15)*100 + (Val/16)*1000;
 i2c_setAck(0);
-Val= i2c_readByte(); //ACK
+Val= i2c_readByte(); 					//ACK
 previous_value=previous_value+ (Val&15)*10000 + (Val/16)*100000;
 i2c_setAck(0);
 
-//------------- читаем содержимое счетчика переполнений ------------
-Val = i2c_readByte(); //ACK
+//---- читаем содержимое счетчика переполнений (counter_overflows)----
+Val = i2c_readByte(); 					//ACK
 counter_overflows = Val;
 i2c_setAck(0);
 
-//------------ читаем содержимое коэффициента деления --------------
-Val = i2c_readByte(); //ACK 
-division_ratio=Val; // Первые 8 бит всунули в 4 байтовое слово
+//----- читаем содержимое коэффициента деления (division_ratio)32-bit  ------
+// пока не используем, просто читаем
+Val = i2c_readByte(); 					//ACK 
+division_ratio=Val; 					// Первые 8 бит всунули в 4 байтовое слово
 i2c_setAck(0);
 Val = i2c_readByte(); //ACK 
 division_ratio=division_ratio+(Val<<8); // Вторые 8 бит всунули в 4 байтовое слово
 i2c_setAck(0);
 Val = i2c_readByte(); //ACK 
-division_ratio=division_ratio+(Val<<16); // третьи 8 бит всунули в 4 байтовое слово
+division_ratio=division_ratio+(Val<<16);// третьи 8 бит всунули в 4 байтовое слово
 i2c_setAck(0);
 Val = i2c_readByte(); //ACK 
-division_ratio=division_ratio+(Val<<24); // Четвартые 8 бит всунули в 4 байтовое слово
+division_ratio=division_ratio+(Val<<24);// Четвертые 8 бит всунули в 4 байтовое слово
 i2c_setAck(0);
 
-//----------- читаем начальное показание счетчика --------------
-Val = i2c_readByte(); //ACK 
-initial_indication=Val; // Первые 8 бит всунули в 4 байтовое слово
+//---- читаем начальное показание счетчика (initial_indication) ----
+// пока не используем, просто читаем
+Val = i2c_readByte(); 					//ACK 
+initial_indication=Val; 				// Первые 8 бит всунули в 4 байтовое слово
 i2c_setAck(0);
-Val = i2c_readByte(); //ACK 
+Val = i2c_readByte(); 					//ACK 
 initial_indication=initial_indication+(Val<<8); // Вторые 8 бит всунули в 4 байтовое слово
 i2c_setAck(0);
-Val = i2c_readByte(); //ACK 
+Val = i2c_readByte(); 					//ACK 
 initial_indication=initial_indication+(Val<<16); // третьи 8 бит всунули в 4 байтовое слово
 i2c_setAck(0);
-Val = i2c_readByte(); //ACK 
-initial_indication=initial_indication+(Val<<24); // Четвартые 8 бит всунули в 4 байтовое слово
-
+Val = i2c_readByte(); 					//ACK 
+initial_indication=initial_indication+(Val<<24); // Четвертые 8 бит всунули в 4 байтовое слово
 //---------------------------------------------------------------------------
 i2c_setAck(1);
 i2c_stop();
-// ------- Теперь проверяем не было ли переполнения -------------------------
-return current_value;
+//return current_value;
 }
 
+// ------- Функция обновления предыдущего значения -------------------
 PCF8583_upd_previous(uint8_t adr)
 {
-i2c_start();			// Устанавливаем на шине Start состояние
-i2c_writeByte(adr*2);	// отправляем байт на шину 0b101000yх (откллюбой RTC)
-  if(i2c_getAck()){return;} 	// slave не откликнулся
-i2c_writeByte(04);		//  устанавливаем адрес регистра 0x00 для чтения
-  if(i2c_getAck()){return;}   // slave не откликнулся
-i2c_writeByte(value_1);	//  записываем по адресу 0x00 байт 0x20
-	if(i2c_getAck()){return;}   // slave не откликнулся
-i2c_writeByte(value_2);	//  записываем по адресу 0x00 байт 0x20
-	if(i2c_getAck()){return;}   // slave не откликнулся
-i2c_writeByte(value_3);	//  записываем по адресу 0x00 байт 0x20
-  if(i2c_getAck()){} 	// slave не откликнулся
-i2c_stop();			// Формируем на шине Stop состояние
+i2c_start();			        	// Устанавливаем на шине Start состояние
+i2c_writeByte(adr*2);	        	// отправляем байт 0b101000yх (откл любой RTC)
+  if(i2c_getAck()){return;} 		// slave не откликнулся
+i2c_writeByte(04);		        	// устанавливаем адрес регистра 0x00 для чтения
+  if(i2c_getAck()){return;}         // slave не откликнулся
+i2c_writeByte(value_1);	        	// записываем по адресу 0x00 байт 0x20
+	if(i2c_getAck()){return;}   	// slave не откликнулся
+i2c_writeByte(value_2);	        	// записываем по адресу 0x00 байт 0x20
+	if(i2c_getAck()){return;}   	// slave не откликнулся
+i2c_writeByte(value_3);	        	// записываем по адресу 0x00 байт 0x20
+  if(i2c_getAck()){} 	           	// slave не откликнулся
+i2c_stop();			        		// Формируем на шине Stop состояние
 }
 
 PCF8583_upd_counter_overflows(uint8_t adr)
 {
-i2c_start();			// Устанавливаем на шине Start состояние
-i2c_writeByte(adr*2);	// отправляем байт на шину 0b101000yх (отк любой RTC)
-  if(i2c_getAck()){return;} 	// slave не откликнулся
-i2c_writeByte(07);		//  устанавливаем адрес регистра 0x00 для чтения
-  if(i2c_getAck()){return;}   // slave не откликнулся
-i2c_writeByte(counter_overflows);	//  записываем по адресу 0x00 байт 0x20
-  if(i2c_getAck()){} 			// slave не откликнулся
-i2c_stop();					// Формируем на шине Stop состояние
+i2c_start();			     		// Устанавливаем на шине Start состояние
+i2c_writeByte(adr*2);	     		// отправл байт на шину 0b101000yх (отк любой RTC)
+  if(i2c_getAck()){return;}      	// slave не откликнулся
+i2c_writeByte(07);		     		// устанавливаем адрес регистра 0x00 для чтения
+  if(i2c_getAck()){return;}      	// slave не откликнулся
+i2c_writeByte(counter_overflows);	// записываем по адресу 0x00 байт 0x20
+  if(i2c_getAck()){} 			 	// slave не откликнулся
+i2c_stop();							// Формируем на шине Stop состояние
 }
 
 void ICACHE_FLASH_ATTR
- timerfunc(uint32_t  timersrc) {
-// выполнение кода каждую 1 секунду
-if(timersrc%10==0){
-// выполнение кода каждые 10 секунд
-PCF8583_read_my (0x50);
-// --- Логика работы счетчика ---------
-
-if (current_value>previous_value)
-{
-	PCF8583_upd_previous(0x50);// Перезапись предыдущего значения счетчика текущим 
-}
-else 
-{ 
-	if(current_value==previous_value)
-	{
-		return;
-	}
-	counter_overflows++;
-	PCF8583_upd_previous(0x50);// Перезапись предыдущего значения счетчика текущим 
-	PCF8583_upd_counter_overflows(0x50);// Перезапись счетчика переполнений counter_overflows
-valdes[0]= initial_indication+(current_value*counter_overflows*10)/division_ratio; //Добавить проверку деления на ноль !!!!!!!!
-}
-}
+ timerfunc(uint32_t  timersrc)
+ {
+      if(timersrc%10==0)  				// выполнение кода каждые 10 секунд
+            { 
+			
+// ---------------- Логика работы счетчика  A -------------------
+				PCF8583_read_my (0x50);
+				
+      if (current_value>previous_value)
+            {       
+				PCF8583_upd_previous(0x50);				// Перезапись предыдущего значения счетчика текущим 
+            }
+      else 
+            { 
+				if(current_value!=previous_value)
+					{
+						counter_overflows++;
+						PCF8583_upd_previous(0x50);				// Перезапись предыдущего значения счетчика текущим 
+						PCF8583_upd_counter_overflows(0x50);	// Перезапись счетчика переполнений counter_overflows
+					}
+			}
+               init1=sensors_param.cfgdes[0];		// Читаем начальное значение из flash памяти esp
+               div1=sensors_param.cfgdes[1];		// Читаем значение коэффициента деления из flash памяти esp
+		if (div1==0) div1=1;   						// исключение деления на ноль
+		counter1_print= init1+((current_value*10+999999*10*counter_overflows)/div1); 
+		valdes[0] = counter1_print;
+// ------ Счетчик B ----------------------------------------
+				current_value=0;					// Обнуляем переменные
+				previous_value=0;					// Обнуляем переменные
+				counter_overflows=0;				// Обнуляем переменные
+				
+				PCF8583_read_my (0x51);
+				
+	if (current_value>previous_value)
+            {       
+				PCF8583_upd_previous(0x51);				// Перезапись предыдущего значения счетчика текущим 
+            }
+      else 
+            { 
+				if(current_value!=previous_value)
+					{
+						counter_overflows++;
+						PCF8583_upd_previous(0x51);				// Перезапись предыдущего значения счетчика текущим 
+						PCF8583_upd_counter_overflows(0x51);	// Перезапись счетчика переполнений counter_overflows
+					}
+			}
+               init1=sensors_param.cfgdes[2];		// Читаем начальное значение из flash памяти esp
+               div1=sensors_param.cfgdes[3];		// Читаем значение коэффициента деления из flash памяти esp
+		if (div1==0) div1=1;   						// исключение деления на ноль
+		counter2_print= init1+((current_value*10+999999*10*counter_overflows)/div1); 
+			valdes[1] = counter2_print;
+			}
 }
 
 void webfunc(char *pbuf) {
-os_sprintf(HTTPBUFF,"Горячая вода: <b>%s</b> ,", fltostr(initial_indication+(current_value*counter_overflows*10)/division_ratio));
-
-os_sprintf(HTTPBUFF,"<br>Счетчик:%d",current_value);
-os_sprintf(HTTPBUFF,"<br>Предыдущее:%d",previous_value);
-os_sprintf(HTTPBUFF,"<br>Начальное:%d",initial_indication);
-os_sprintf(HTTPBUFF,"<br>Делитель:%d",division_ratio);
-os_sprintf(HTTPBUFF,"<br>Переполнения:%d",counter_overflows);
+os_sprintf(HTTPBUFF,"Горячая вода: <b>%s</b> ,", fltostr(counter1_print));
+os_sprintf(HTTPBUFF,"<br>Показания счетчика A: %d",counter1_print);
+os_sprintf(HTTPBUFF,"<br>Показания счетчика B: %d",counter2_print);
+os_sprintf(HTTPBUFF,"<br>Текущее значение:%d",current_value);
+os_sprintf(HTTPBUFF,"<br>Предыдущее значение:%d",previous_value);
+os_sprintf(HTTPBUFF,"<br>Счетчик переполнений:%d",counter_overflows);
 }

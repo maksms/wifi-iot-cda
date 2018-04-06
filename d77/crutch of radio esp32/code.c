c1f197e602974be125e03b4a4d22526c2a07b96b
Dmitry <dlypokupo@mail.ru>
2018-04-06 16:51:46 +0300

    Update code.c

--- d77/crutch of radio esp32/code.c
+++ d77/crutch of radio esp32/code.c
@@ -1,6 +1,6 @@
 /*
-	в конструкторе кода перед сборкой прошивки в окно "Глобальные переменные" вставить 2 ,а 
-    в окно "Количество настроек" этот текст без кавычек : "
+	в конструкторе кода перед сборкой прошивки в окно "Глобальные переменные" вставить "1" ,а 
+    	в окно "Количество настроек" этот текст без кавычек : "
 	
 	стартовая станция,стартовая громкость,время до старта радио после перезагрузки,время через которое вернет станцию при обрыве,предел памяти,время сброса по памяти
 	
@@ -9,7 +9,7 @@
 uint32_t counter_of_time_of_free_memory = 0;
 void startfunc()
 {
-	valdes[0] = valdes[1] = 0;	
+	valdes[0] = 0;	
 }
 void timerfunc(uint32_t timersrc)
 {
@@ -25,21 +25,24 @@ void timerfunc(uint32_t timersrc)
 		sensors_param.cfgdes[4] = 120000; //предел памяти
 	if (sensors_param.cfgdes[5] < 60 || 200000 < sensors_param.cfgdes[5])
 		sensors_param.cfgdes[5] = 300; //время сброса по памяти
-	if (curradio && valdes[0] != curradio)
-		valdes[0] = curradio; //сохраняем состояние
+	if (curradio && sensors_param.cfgdes[0] != curradio && sensors_param.cfgdes[0])
+	{
+		sensors_param.cfgdes[0] = curradio; //сохраняем состояние
+		SAVEOPT
+	}
 	if (curradio !=0 && system_get_free_heap_size() > sensors_param.cfgdes[4])
 		counter_of_time_of_free_memory ++;
 	else
 		counter_of_time_of_free_memory = 0;
 	if (counter_of_time_of_free_memory > sensors_param.cfgdes[5])
 		system_restart();
-	if (!curradio && valdes[0] && pt2257_vol_state)
-		valdes[1]++;
-	if (valdes[1] > sensors_param.cfgdes[3])
+	if (!curradio && sensors_param.cfgdes[0] && pt2257_vol_state)
+		valdes[0]++;
+	if (valdes[0] > sensors_param.cfgdes[3])
 	{
-		os_sprintf(urlbuf, "%d", valdes[0]);
+		os_sprintf(urlbuf, "%d", sensors_param.cfgdes[0]);
 		radio_control();
-		valdes[1] = 0;
+		valdes[0] = 0;
 	}
 	vTaskDelay(1000 / portTICK_PERIOD_MS);
 }

286c81495d7e99efb7c28af8f49c30bd6bfbb0a3
d51x <dimonich110@gmail.com>
2021-01-14 00:19:32 +0300

    hall2: упростил задание параметров, каждый параметр отдельная опция, для каждого канала pwm сделал свой таймер

--- d51x/esp_hall/code_v4_pir_led.c
+++ d51x/esp_hall/code_v4_pir_led.c
@@ -1,128 +1,137 @@
-#define FW_VER "4.0"
+#define FW_VER "4.1"
 
-// Длительность, Уровень яркости, Мин. освещенность, Задержка выкл PIR, PIR switch VGPIO
+// Настройки: PIR1 GPIO, active level, func type, func GPIO/CH,OFF delay, Signal GPIO, NightMode,
+// Настройки: PIR2 GPIO, active level, func type, func GPIO/CH,OFF delay, Signal GPIO, NightMode,
+// Настройки: DryCont1 GPIO, active level, func type, func GPIO/CH,OFF delay, Signal GPIO, NightMode,
+// Настройки: DryCont2 GPIO, active level, func type, func GPIO/CH,OFF delay, Signal GPIO, NightMode,
+// Настройки: Led Night GPIO, PwmStepSpeed
+
+// Настройки: PIR1 GPIO, active level, func type, func GPIO/CH,OFF delay, Signal GPIO, NightMode,PIR2 GPIO, active level, func type, func GPIO/CH,OFF delay, Signal GPIO, NightMode,DryCont1 GPIO, active level, func type, func GPIO/CH,OFF delay, Signal GPIO, NightMode,DryCont2 GPIO, active level, func type, func GPIO/CH,OFF delay, Signal GPIO, NightMode, Led Night GPIO,PwmStepSpeed
 
 
 /*
 *   v.4 changes:
-
-
+		* перевел в концепцию 4 сухих контакта с возможностью для каждого задать: 
+			- gpio сухого контакта 
+			- активный уровень gpio контакта
+			- функцию, которую выполняет контакт:
+						0 - вкл/выкл gpio
+						1 - вкл/выкл pwm (плавно с текущего duty до указанного, см. ниже)
+						2 - переключить gpio
+			- номер gpio или канала pwm, который будет управляться сигналом с контакта
+			- задержку выключения gpio или канала
+			- контакт включает указанный gpio или канал pwm только в темноте (с датчика освещенности)
+			- номер gpio для включения сигнального светодиода, что контакт сработал (при выключении задержка не используется, см. выше)
+			
+		* возможность задать мин и макс освещенность, при которой осуществляется определение темно/светло (задается в нтерпретере)
+		* возможность задать время начала и окончания ночного режима, при котором яркость минимальна (время и значение яркости задается через интерпретер)
+	
+		//TODO: через конструктор строк задать конфигурацию сенсора ( t:g;v:221;d:1;s:220 )
 */
-// TODO: 
-// для "Датчик Движения 1" можно задать gpio или pwm, можно задать время таймера на отключение, сработает при пропаже сигнала (нет движения)
-// для "Датчик Движения 2" можно задать gpio или pwm, можно задать время таймера на отключение, сработает при пропаже сигнала (нет движения)
-// для "Датчик Геркон 1" можно задать gpio или pwm, можно задать время таймера на отключение, сработает при пропаже сигнала (контакт закрыт)
-// для "Датчик Геркон 2" можно задать gpio или pwm, можно задать время таймера на отключение, сработает при пропаже сигнала (контакт закрыт)
 
-// уровень яркости ленты в обычном режиме 
-// уровень яркости ленты в ночном режиме (задается время с и время по), лучше через интерпретер и установку valdes - и время с и время по и урвовень днем и ночью будет региулироваться
 
-#define GPIO_LED_STRIP 13
-//#define GPIO_REED_SW_1 4
-//#define GPIO_REED_SW_2 5
-#define GPIO_PIR_1 4
-//#define GPIO_PIR_1 12
-//#define GPIO_PIR_2 14
-#define PWM_LED 0
-
-#define GPIO_BTN_1 222
-#define GPIO_BTN_2 223
-#define GPIO_BTN_3 227
-
-#define GPIO_LED_NIGHT 224
-
-#define GPIO_LED_PIR_1 15
-//#define GPIO_LED_PIR_1 220
-#define GPIO_LED_PIR_2 221
+#define MQTT_TOPIC_NIGHT_MODE 			"night-mode"
 
 #define NO_GPIO 255
 //#define GPIO_LED_GREEN 226
 //#define GPIO_LED_RED 225
 
 #define READ_SENSORS_INTERVAL 20
-//#define READ_BUTTONS_INTERVAL 300
 
-
-#define LUMINOSITY_STEP 20
-#define LBRIGHTNESS_STEP_DELAY 20
-#define LIGHT_DELAY_STEP 5
-
-#define MIN_LIGHT_DELAY 5
-#define MAX_LIGHT_DELAY 50
-
-#define PIR_DELAY_OFF 30000
 
 #define CFG_SENSOR_FUNC_IDX				0
-#define CFG_PIR1_FUNC					sensors_param.cfgdes[0]			// функция для PIR1, 0-255: GPIO, 1000-1004: PWM
-#define CFG_PIR1_DELAY					sensors_param.cfgdes[1]			// задержка выключения pir1 (запускается таймер)
+#define CFG_SENSOR_DATA_SIZE			7
 
-#define CFG_PIR2_FUNC					sensors_param.cfgdes[2]			// функция для PIR2, 0-255: GPIO, 1000-1004: PWM
-#define CFG_PIR2_DELAY					sensors_param.cfgdes[3]			// задержка выключения pir2 (запускается таймер)
+#define CFG_PIR1_GPIO					sensors_param.cfgdes[0]			// 12		
+#define CFG_PIR1_LEVEL 					sensors_param.cfgdes[1]			// 1	//уровень срабатывания
+#define CFG_PIR1_FUNC					sensors_param.cfgdes[2]			// функция для PIR1, 0: GPIO, 1: PWM; 2: toggle GPIO
+#define CFG_PIR1_PIN_CH					sensors_param.cfgdes[3]			// номер gpio или канала pwm
+#define CFG_PIR1_DELAY					sensors_param.cfgdes[4]			// задержка выключения pir1 (запускается таймер)
+#define CFG_PIR1_SIGNAL_GPIO			sensors_param.cfgdes[5]			// 220 // gpio для сигнализации сработки датчика
+#define CFG_PIR1_NIGHT_MODE				sensors_param.cfgdes[6]			// вызов on-callback только в темноте
 
-#define CFG_RSW1_FUNC					sensors_param.cfgdes[4]			// функция для Reed Switch 1, 0-255: GPIO, 1000-1004: PWM
-#define CFG_RSW1_DELAY					sensors_param.cfgdes[5]			// задержка выключения ReedSwitch1 (запускается таймер)
 
-#define CFG_RSW2_FUNC					sensors_param.cfgdes[6]			// функция для Reed Switch 2, 0-255: GPIO, 1000-1004: PWM
-#define CFG_RSW2_DELAY					sensors_param.cfgdes[7]			// задержка выключения ReedSwitch2  (запускается таймер)
+#define CFG_PIR2_GPIO					sensors_param.cfgdes[7]			// 14
+#define CFG_PIR2_LEVEL					sensors_param.cfgdes[8]			// 1	//уровень срабатывания
+#define CFG_PIR2_FUNC					sensors_param.cfgdes[9]			// функция для PIR1, 0: GPIO, 1: PWM
+#define CFG_PIR2_PIN_CH					sensors_param.cfgdes[10]			// номер gpio или канала pwm
+#define CFG_PIR2_DELAY					sensors_param.cfgdes[11]			// задержка выключения pir2 (запускается таймер)
+#define CFG_PIR2_SIGNAL_GPIO			sensors_param.cfgdes[12]		// 221	// gpio для сигнализации сработки датчика
+#define CFG_PIR2_NIGHT_MODE				sensors_param.cfgdes[13]			// вызов on-callback только в темноте
 
-#define CFG_BRIGHTNESS_LEVEL_MIN		sensors_param.cfgdes[8]			// минимальная яркость в заданное (ночное время), выставляем через интерпретер valdes[0]
+#define CFG_DRY1_GPIO					sensors_param.cfgdes[14] 		// 4
+#define CFG_DRY1_LEVEL 					sensors_param.cfgdes[15] 		// 0	// уровень срабатывания
+#define CFG_DRY1_FUNC					sensors_param.cfgdes[16]		// функция для DRY1, 0: GPIO, 1: PWM
+#define CFG_DRY1_PIN_CH					sensors_param.cfgdes[17]		// номер gpio или канала pwm
+#define CFG_DRY1_DELAY					sensors_param.cfgdes[18]		// задержка выключения DryContact1 (запускается таймер)
+#define CFG_DRY1_SIGNAL_GPIO			sensors_param.cfgdes[19]		// 255 // gpio для сигнализации сработки датчика
+#define CFG_DRY1_NIGHT_MODE				sensors_param.cfgdes[20]		// вызов on-callback только в темноте
 
-#define CFG_BRIGTHNESS_STEP_DELAY		sensors_param.cfgdes[9]
+#define CFG_DRY2_GPIO					sensors_param.cfgdes[21] 		// 5
+#define CFG_DRY2_LEVEL					sensors_param.cfgdes[22] 		// 0	//уровень срабатывания
+#define CFG_DRY2_FUNC					sensors_param.cfgdes[23]		// функция для DRY2, 0: GPIO, 1: PWM
+#define CFG_DRY2_PIN_CH					sensors_param.cfgdes[24]		// номер gpio или канала pwm
+#define CFG_DRY2_DELAY					sensors_param.cfgdes[25]		// задержка выключения DryContact2  (запускается таймер)
+#define CFG_DRY2_SIGNAL_GPIO			sensors_param.cfgdes[26]		// 255 // gpio для сигнализации сработки датчика			
+#define CFG_DRY2_NIGHT_MODE				sensors_param.cfgdes[27]		// вызов on-callback только в темноте
 
-#define VALDES_IS_DARK					valdes[0]
+#define GPIO_LED_NIGHT					sensors_param.cfgdes[28]		//224	// gpio для сигнализации темного времени
+#define CFG_BRIGTHNESS_STEP_DELAY		sensors_param.cfgdes[29]		// скорость шага pwm
 
+#define VALDES_IS_DARK					valdes[0]						// выставляется через interpreter по гистерезису освещенности
+#define VALDES_MIN_NIGHT_BRIGTHNESS		valdes[1]						// выставляется через interpreter по времени
 
 //#define BRIGHTNESS_STEPS 20
 //static uint8_t brightness[BRIGHTNESS_STEPS] = {0,1,2,3,4,5,7,9,12,16,21,28,37,48,64,84,111,147,194,255};  
 #define BRIGHTNESS_STEPS 32
 static uint8_t brightness[BRIGHTNESS_STEPS] = {0,1,2,3,5,8,12,16,21,26,32,38,45,52,60,68,76,85,95,105,115,125,136,148,160,172,185,198,211,225,239,255};
 
-static uint8_t light_delay = 25; // msec, длительность свечения каждой ступени яркости
+#define BRIGHTNESS_STEP_DELAY 20 
+static uint8_t light_delay = BRIGHTNESS_STEP_DELAY; // msec, длительность свечения каждой ступени яркости
 
 typedef void (* func_cb)(void *args);  
 
 typedef enum {
 	  USER_SENSOR_PIR1		// level = 1
-//	, USER_SENSOR_PIR2		// level = 1
-//	, USER_SENSOR_RSW1		// level = 0
-//	, USER_SENSOR_RSW2		// level = 0
+	, USER_SENSOR_PIR2		// level = 1
+	, USER_SENSOR_RSW1		// level = 0
+	, USER_SENSOR_RSW2		// level = 0
 	, USER_SENSOR_MAX
 } user_sensor_type_e;
 
 typedef enum {
 	USER_FUNC_GPIO,
-	USER_FUNC_PWM
+	USER_FUNC_PWM,
+	USER_FUNC_TOGGLE_GPIO
 } user_sensor_func_e;
 
 typedef struct {
+	uint8_t state;
 	uint8_t gpio;
+	uint8_t active_level;
 	uint8_t signal_gpio;
-	uint8_t prev_state;				// предыдущее состояние
 	user_sensor_func_e func;		// тип функции сенсора - gpio или pwm
 	uint16_t pin_ch;				// номер gpio или номер channel pwm
-	uint8_t level;					// уровень, при котором состояние ON
+
 	char *topic;
 	uint32_t tmr_delay;				// через какое время сработает таймер при закрытии контакта / пропадании сигнала
 	func_cb tmr_cb;						// callback таймера
 	void *tmr_args;						// аргументы коллбека
 	func_cb on;						// callback при ON
 	void *on_args;						// аргументы коллбека
+	uint8_t dark;						// on-callback сработает только в темноте
 	func_cb off;						// callback при OFF
 	void *off_args;						// аргументы коллбека
-	os_timer_t tmr;					// сам таймер
+	os_timer_t tmr;					// сам таймер задержки
+	os_timer_t tmr_pwm; 			// таймер для pwm функции (func)
+
 } contact_sensor_t;
 
-contact_sensor_t _sensors[USER_SENSOR_MAX] = {
-	  {GPIO_PIR_1,		GPIO_LED_PIR_1,	0,	USER_FUNC_GPIO,	NO_GPIO,	0,	"pir-1",	0,	NULL,	NULL,	NULL,	NULL, 	NULL, 	NULL,	0}
-	//, {GPIO_PIR_2,		GPIO_LED_PIR_2,	0,	USER_FUNC_GPIO,	NO_GPIO,	1,	"pir-2",	0,	NULL,	NULL,	NULL,	NULL, 	NULL, 	NULL,	0}
-	//, {GPIO_REED_SW_1,	NO_GPIO,		0,	USER_FUNC_GPIO,	NO_GPIO,	0,	"rsw-1",	0,	NULL,	NULL,	NULL,	NULL, 	NULL, 	NULL,	0}
-	//, {GPIO_REED_SW_2,	NO_GPIO,		0,	USER_FUNC_GPIO,	NO_GPIO,	0,	"rsw-2",	0,	NULL,	NULL,	NULL,	NULL, 	NULL, 	NULL,	0}
-};
+contact_sensor_t _sensors[USER_SENSOR_MAX];
 
 static volatile os_timer_t timer_read_sensors; 
-static volatile os_timer_t timer_pwm; 
 
-static uint8_t is_dark = 0;
+static uint8_t is_dark = 0;			// флан ночного режима по датчbre освещенности, при 1 - включаем gpio/pwm
 
 static void ICACHE_FLASH_ATTR mqtt_send_int(const char *topic, int val)
 {
@@ -142,15 +151,15 @@ void ICACHE_FLASH_ATTR read_sensors_cb()
 	for (i = 0; i < USER_SENSOR_MAX; i++ )
 	{
 		uint8_t state = GPIO_ALL_GET(_sensors[i].gpio);
-		state = ( _sensors[i].level ) ? state : !state;
-		if ( state == _sensors[i].prev_state ) continue;  // состояние не менялось
+		state = ( _sensors[i].active_level ) ? state : !state;
+		if ( state == _sensors[i].state ) continue;  // состояние не менялось
 	
-		//uint8_t _state = ( _sensors[i].level ) ? state : !state;
+		//uint8_t _state = ( _sensors[i].active_level ) ? state : !state;
 		
 		// состояние изменилось, выполнить callback
 		if ( state ) 
 		{
-			if ( _sensors[i].on != NULL )
+			if ( _sensors[i].on != NULL && ( !_sensors[i].dark || ( _sensors[i].dark && is_dark) ) )  // + включаем только в темноте, а выключаем в любое время
 				_sensors[i].on( _sensors[i].on_args );
 				
 		} 
@@ -168,34 +177,41 @@ void ICACHE_FLASH_ATTR read_sensors_cb()
 			}				
 		}
 		
-		mqtt_send_int(_sensors[i].topic, state);
-		_sensors[i].prev_state = state;
+		mqtt_send_int(_sensors[i].topic, state); // сразу же отправим данные в топик, только если состояние изменилось
+		_sensors[i].state = state;
 		
 		// set signal led gpio
-		//if ( _sensors[i].signal_gpio != NO_GPIO )
-		//	GPIO_ALL( _sensors[i].signal_gpio, state );		
+		if ( _sensors[i].signal_gpio != NO_GPIO )
+			GPIO_ALL( _sensors[i].signal_gpio, state );		
 	}
 	
 }
 
+void ICACHE_FLASH_ATTR gpio_toggle_cb(void *args)
+{
+	contact_sensor_t *_sens = (contact_sensor_t *)args;
+	uint8_t state = GPIO_ALL_GET(_sens->pin_ch);	
+	GPIO_ALL(_sens->pin_ch, !state);	
+}
 
 void ICACHE_FLASH_ATTR gpio_on_cb(void *args)
 {
-	uint16_t *gpio = (uint16_t *)args;
-	GPIO_ALL(*gpio, 1);
+	contact_sensor_t *_sens = (contact_sensor_t *)args;
+	GPIO_ALL(_sens->pin_ch, 1);	
 }
 
 void ICACHE_FLASH_ATTR gpio_off_cb(void *args)
 {
-	uint16_t *gpio = (uint16_t *)args;
-	GPIO_ALL(*gpio, 0);	
+	contact_sensor_t *_sens = (contact_sensor_t *)args;
+	GPIO_ALL(_sens->pin_ch, 0);	
 }
 
 void ICACHE_FLASH_ATTR led_on_cb(void *args)
 {
-	uint16_t *ch = (uint16_t *)(args);
-	uint8_t duty = pwm_get_duty_iot( *ch );
-
+	contact_sensor_t *_sens = (contact_sensor_t *)args;
+	
+	uint8_t duty = pwm_get_duty_iot( _sens->pin_ch );
+			
 	// получить ближайшее максимальное значение по таблице
 	uint8_t i;
 	for ( i=0; i < BRIGHTNESS_STEPS; i++)
@@ -206,22 +222,30 @@ void ICACHE_FLASH_ATTR led_on_cb(void *args)
 	
 	if ( i == BRIGHTNESS_STEPS ) return;
 	
+	
+
+	uint8_t new_duty;
 	if ( brightness[i] == duty )
-		pwm_set_duty_iot( brightness[i+1], *ch);
+		new_duty = brightness[i+1];
 	else	
-		pwm_set_duty_iot( brightness[i], *ch);
+		new_duty = 	brightness[i];
+		
+	if ( new_duty > VALDES_MIN_NIGHT_BRIGTHNESS )
+		new_duty = VALDES_MIN_NIGHT_BRIGTHNESS;
+		
+	pwm_set_duty_iot( new_duty, _sens->pin_ch);
 		
 	pwm_start_iot();
 	
-	os_timer_disarm(&timer_pwm);
-	os_timer_setfn(&timer_pwm, (os_timer_func_t *) led_on_cb, ch);
-	os_timer_arm(&timer_pwm, light_delay, 0);
+	os_timer_disarm(&_sens->tmr_pwm);
+	os_timer_setfn(&_sens->tmr_pwm, (os_timer_func_t *) led_on_cb, _sens);
+	os_timer_arm(&_sens->tmr_pwm, light_delay, 0);
 }
 
 void ICACHE_FLASH_ATTR led_off_cb(void *args)
 {
-	uint16_t *ch = (uint16_t*)(args);
-	uint8_t duty = pwm_get_duty_iot( *ch );
+	contact_sensor_t *_sens = (contact_sensor_t *)args;
+	uint8_t duty = pwm_get_duty_iot( _sens->pin_ch );
 	
 	// получить ближайшее минимальное значение по таблице
 	uint8_t i;
@@ -232,12 +256,12 @@ void ICACHE_FLASH_ATTR led_off_cb(void *args)
 	}	
 	if ( i == 0 ) return;
 	
-	pwm_set_duty_iot( brightness[i-1], *ch);
+	pwm_set_duty_iot( brightness[i-1], _sens->pin_ch);
 	pwm_start_iot();
 	
-	os_timer_disarm(&timer_pwm);
-	os_timer_setfn(&timer_pwm, (os_timer_func_t *) led_off_cb, ch);
-	os_timer_arm(&timer_pwm, light_delay, 0);	
+	os_timer_disarm(&_sens->tmr_pwm);
+	os_timer_setfn(&_sens->tmr_pwm, (os_timer_func_t *) led_off_cb, _sens);
+	os_timer_arm(&_sens->tmr_pwm, light_delay, 0);	
 }
 
 void ICACHE_FLASH_ATTR load_options()
@@ -246,44 +270,70 @@ void ICACHE_FLASH_ATTR load_options()
 	uint8_t i;
 	for ( i = 0; i < USER_SENSOR_MAX; i++ )
 	{
-		_sensors[i].func = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*2] / 1000;
-		_sensors[i].pin_ch = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*2] % 1000;
-		_sensors[i].tmr_delay = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*2 + 1];
+
+		_sensors[i].gpio = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*CFG_SENSOR_DATA_SIZE];
+		_sensors[i].active_level = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*CFG_SENSOR_DATA_SIZE + 1];
+		_sensors[i].func = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*CFG_SENSOR_DATA_SIZE + 2];
+		_sensors[i].pin_ch = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*CFG_SENSOR_DATA_SIZE + 3];
+		_sensors[i].tmr_delay = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*CFG_SENSOR_DATA_SIZE + 4];
+		_sensors[i].signal_gpio = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*CFG_SENSOR_DATA_SIZE + 5];
+		_sensors[i].dark = sensors_param.cfgdes[CFG_SENSOR_FUNC_IDX + i*CFG_SENSOR_DATA_SIZE + 6];
+		
+		
 		if ( _sensors[i].func == USER_FUNC_GPIO )
 		{
 			_sensors[i].on = gpio_on_cb;
-			_sensors[i].on_args = &_sensors[i].pin_ch;
+			_sensors[i].on_args = &_sensors[i];
 			
 			if ( _sensors[i].tmr_delay == 0 )
 			{
 				_sensors[i].off = gpio_off_cb;
-				_sensors[i].off_args = &_sensors[i].pin_ch;
+				_sensors[i].off_args = &_sensors[i];
 				_sensors[i].tmr_cb == NULL;
 			} else {
 				_sensors[i].tmr_cb = gpio_off_cb;
-				_sensors[i].tmr_args = &_sensors[i].pin_ch;
+				_sensors[i].tmr_args = &_sensors[i];
 				_sensors[i].off = NULL;
 			}		
 		}
 		else if ( _sensors[i].func == USER_FUNC_PWM )
 		{
 			_sensors[i].on = led_on_cb;
-			_sensors[i].on_args = &_sensors[i].pin_ch;
+			_sensors[i].on_args = &_sensors[i];
 			
 			if ( _sensors[i].tmr_delay == 0 )
 			{
 				_sensors[i].off = led_off_cb;
-				_sensors[i].off_args = &_sensors[i].pin_ch;							
+				_sensors[i].off_args = &_sensors[i];							
 				_sensors[i].tmr_cb == NULL;
 			} else {
 				_sensors[i].tmr_cb = led_off_cb;
-				_sensors[i].tmr_args = &_sensors[i].pin_ch;							
+				_sensors[i].tmr_args = &_sensors[i];							
 				_sensors[i].off == NULL;				
 			}	
 		}
+		else if ( _sensors[i].func == USER_FUNC_TOGGLE_GPIO )
+		{
+			_sensors[i].on = gpio_toggle_cb;
+			_sensors[i].on_args = &_sensors[i];
+			
+			if ( _sensors[i].tmr_delay == 0 )
+			{
+				_sensors[i].off = gpio_toggle_cb;
+				_sensors[i].off_args = &_sensors[i];
+				_sensors[i].tmr_cb == NULL;
+			} else {
+				_sensors[i].tmr_cb = gpio_toggle_cb;
+				_sensors[i].tmr_args = &_sensors[i];
+				_sensors[i].off = NULL;
+			}				
+		}
 	}	
 	
-	light_delay = CFG_BRIGTHNESS_STEP_DELAY < 10 ? LBRIGHTNESS_STEP_DELAY : CFG_BRIGTHNESS_STEP_DELAY;
+	
+
+	
+	light_delay = CFG_BRIGTHNESS_STEP_DELAY < 10 ? BRIGHTNESS_STEP_DELAY : CFG_BRIGTHNESS_STEP_DELAY;
 }
 
 void ICACHE_FLASH_ATTR startfunc()
@@ -296,6 +346,11 @@ void ICACHE_FLASH_ATTR startfunc()
 	
 	load_options();
 	
+	_sensors[0].topic = "pir-1";
+	_sensors[1].topic = "pir-2";
+	_sensors[2].topic = "rsw-1";
+	_sensors[3].topic = "rsw-2";
+	
 	os_timer_disarm(&timer_read_sensors);
 	os_timer_setfn(&timer_read_sensors, (os_timer_func_t *) read_sensors_cb, NULL);
 	os_timer_arm(&timer_read_sensors, READ_SENSORS_INTERVAL, 1);
@@ -304,23 +359,33 @@ void ICACHE_FLASH_ATTR startfunc()
 
 void ICACHE_FLASH_ATTR timerfunc(uint32_t  timersrc) {
 	// выполнение кода каждую 1 секунду
+	load_options();
+	
 	if( timersrc % 10 == 0 ){  // каждые 10 сек
-		load_options();
-		mqtt_send_int("night-mode", is_dark);
+		
 	} 
 	
+	if( timersrc % 60 == 0 ){  // каждые 60 сек
+		mqtt_send_int(MQTT_TOPIC_NIGHT_MODE, is_dark);
+	} 
 	
 	// через интерпретер с учетом гистерезиса при чтении adc выставляем valdes[0] // VALDES_IS_DARK // 
 	GPIO_ALL(GPIO_LED_NIGHT, VALDES_IS_DARK );  // подсвечиваем
 	if ( VALDES_IS_DARK != is_dark )
 	{
 		is_dark = VALDES_IS_DARK;
-		mqtt_send_int("night-mode", is_dark);
+		mqtt_send_int(MQTT_TOPIC_NIGHT_MODE, is_dark); // отправка при изменениее
 	}		
+	
+	// VALDES_MIN_NIGHT_BRIGTHNESS 
 }
 
 
 
 void webfunc(char *pbuf) {
-	
+	uint8_t i;
+	for ( i = 0; i < USER_SENSOR_MAX; i++ )
+	{
+		os_sprintf(HTTPBUFF, "%s = %d<br>", _sensors[i].topic, _sensors[i].state );
+	}		
 }

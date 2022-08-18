0fe30a73e92dc3da7a06a1463ac23974ff2112e7
Vladi <45065322+zorketRU@users.noreply.github.com>
2022-08-18 12:59:56 +0300

    Update second_ticker.c

--- zorketRU/Examples/second_ticker.c
+++ zorketRU/Examples/second_ticker.c
@@ -1,16 +1,19 @@
-uint32_t ticker = 0;
+uint32_t sec1_ticker = 0;
+uint32_t sec30_ticker = 0;
 
 void startfunc(){// выполняется один раз при старте модуля.
 }
 
 void timerfunc(uint32_t  timersrc) {// выполнение кода каждую 1 секунду
 
-ticker++;
+sec1_ticker++;
 
 if(timersrc%30==0){// выполнение кода каждые 30 секунд
+ sec30_ticker++;
 }
  delay(1000); // обязательная строка, минимальное значение для RTOS систем- 10мс
 }
 void webfunc(char *pbuf) {
-os_sprintf(HTTPBUFF,"Натикало %d", ticker); // вывод данных на главной модуля
+os_sprintf(HTTPBUFF,"1-секундных запусков кода %d:", ticker); // вывод данных на главной модуля
+os_sprintf(HTTPBUFF,"30-секундных запусков кода%d:", ticker); // вывод данных на главной модуля
 }
